94a2ab620e41a06fd11a4c8b367e3789f9d3da5d
joaochioda <joao.benetasso@taglivros.com>
2022-02-28 02:28:02 -0300

    init

---
+++ src/components/connectors/MetaMaskCard.jsx
@@ -0,0 +1,46 @@
+import { hooks, metaMask } from './metaMask.js'
+import { Accounts } from '../Accounts'
+import { Card } from '../Card'
+import { Chain } from '../Chain'
+import { ConnectWithSelect } from '../ConnectWithSelect'
+import { Status } from '../Status'
+
+const { useChainId, useAccounts, useError, useIsActivating, useIsActive, useProvider, useENSNames, useWeb3React } = hooks
+
+export default function MetaMaskCard() {
+  const chainId = useChainId()
+  const accounts = useAccounts()
+  const error = useError()
+  const isActivating = useIsActivating()
+
+  const isActive = useIsActive()
+
+  const provider = useProvider()
+  const ENSNames = useENSNames(provider)
+  const { library } = useWeb3React(provider);
+  function sig() {
+    library?.getSigner().signMessage('Message').then(console.log)
+  }
+
+  return (
+    <Card>
+      <div>
+        <b>MetaMask</b>
+        <Status isActivating={isActivating} error={error} isActive={isActive} />
+        <div style={{ marginBottom: '1rem' }} />
+        <Chain chainId={chainId} />
+        <Accounts accounts={accounts} provider={provider} ENSNames={ENSNames} />
+      </div>
+      <button onClick={sig}>Sign</button>
+      <div style={{ marginBottom: '1rem' }} />
+      
+      <ConnectWithSelect
+        connector={metaMask}
+        chainId={chainId}
+        isActivating={isActivating}
+        error={error}
+        isActive={isActive}
+      />
+    </Card>
+  )
+}
